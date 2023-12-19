db0684fa357baec5dbfd7b63cfae96031bc33028
wodnd0411 <wodnd0411@naver.com>
2023-12-19 14:32:26 +0900

    연습문제 ch04

--- 연습문제/src/ch04/Exercise4_12.java
+++ 연습문제/src/ch04/Exercise4_12.java
@@ -3,13 +3,6 @@ package ch04;
 public class Exercise4_12 {
 
 	public static void main(String[] args) {
-		for(int i = 2; i <= 9; i++) {
-			for(int j = 1; j <= 3; j++) {
-				System.out.println(i + " X " + j + " = " + i * j);
-			}
-			
-			System.out.println();
-		}
+		
 	}
-
 }
